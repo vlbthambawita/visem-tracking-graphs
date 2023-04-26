d8d142d33cf8f9f2d557713dff8f7b046ab7d3df
vlbthambawita <vlbthambawita@gmail.com>
2023-04-26 12:30:00 +0000

    complete script

--- generate_graphs_v1.py
+++ generate_graphs_v1.py
@@ -11,16 +11,16 @@ def load_data_from_file(file_path, frame_number):
         for line in f:
             # Assuming each line in the text file is in the format: sperm_id class x_center y_center width height
             sperm_id, class_name, x_center, y_center, width, height = line.strip().split()
-            data.append((int(sperm_id), class_name, (float(x_center), float(y_center), float(width), float(height)), frame_number))
+            data.append((sperm_id, str(class_name), [float(x_center), float(y_center), float(width), float(height)], frame_number))
     return data
 
 # Set the directory containing the text files
-data_directory = "/work/vajira/DATA/VISEM-Tracking-from_kaggle/VISEM_Tracking_Train_v4/Train/11/labels_ftid"
+data_directory = "/work/vajira/DATA/VISEM-Tracking-from_kaggle/VISEM_Tracking_Train_v4/Train/23/labels_ftid"
 
 # Load the dataset from the text files
 dataset = []
 for file_name in os.listdir(data_directory):
-    frame_number = int(file_name.split('.')[0])  # Assuming the frame number is the first part of the file name
+    frame_number = int(file_name.split('.')[0].split("_")[2])  # Assuming the frame number is the first part of the file name
     file_path = os.path.join(data_directory, file_name)
     
     dataset.extend(load_data_from_file(file_path, frame_number))
@@ -34,7 +34,9 @@ number_of_frames = count_files(data_directory)  # Set the total number of frames
 frame_graphs = [nx.Graph() for _ in range(number_of_frames)]
 
 for sperm_id, class_name, yolo_coordinates, frame_number in dataset:
-    frame_graphs[frame_number].add_node(sperm_id, class_name=class_name, coordinates=yolo_coordinates)
+    #frame_graphs[frame_number].add_node(sperm_id, class_name=str(class_name), coordinates=list(yolo_coordinates))
+    frame_graphs[frame_number].add_node(sperm_id, class_name=class_name, x_center=yolo_coordinates[0], y_center=yolo_coordinates[1], width=yolo_coordinates[2], height=yolo_coordinates[3])
+
 
 spatial_threshold = 0.25 # Set an appropriate threshold based on your data, this is based on the YOLO coordinates
 
@@ -42,12 +44,14 @@ for frame_graph in frame_graphs:
     for sperm_id1, data1 in frame_graph.nodes(data=True):
         for sperm_id2, data2 in frame_graph.nodes(data=True):
             if sperm_id1 != sperm_id2:
-                center1 = (data1['coordinates'][0], data1['coordinates'][1])
-                center2 = (data2['coordinates'][0], data2['coordinates'][1])
+                #center1 = (data1['coordinates'][0], data1['coordinates'][1])
+                #center2 = (data2['coordinates'][0], data2['coordinates'][1])
+                center1 = (data1['x_center'], data1['y_center'])
+                center2 = (data2['x_center'], data2['y_center'])
                 distance = euclidean_distance(center1, center2)
 
                 if distance < spatial_threshold:
-                    frame_graph.add_edge(sperm_id1, sperm_id2, weight=distance)
+                    frame_graph.add_edge(sperm_id1, sperm_id2, weight=distance, edge_type="spatial")
 
 video_graph = nx.DiGraph()
 
@@ -62,14 +66,15 @@ for frame_number in range(len(frame_graphs) - 1):
     for sperm_id1 in frame1_nodes:
         for sperm_id2 in frame2_nodes:
             if sperm_id1 == sperm_id2:  # Same sperm_id across consecutive frames
-                video_graph.add_edge(sperm_id1, sperm_id2)
+                video_graph.add_edge(sperm_id1, sperm_id2, edge_type="temporal")
 
 
 # Save the graphs to disk
 import os
 
 # Set the directory where you want to save the graphs
-output_directory = "/work/vajira/DATA/visem-traking-graphs/11"
+output_directory = "/work/vajira/DATA/visem-traking-graphs/23"
+os.makedirs(output_directory, exist_ok=True)
 
 # Save the individual frame graphs
 for i, frame_graph in enumerate(frame_graphs):
